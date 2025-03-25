66a1ad2e2813d221546bc6cf84d212a084dba989
Diego Villaseñor <diego.vid.eco@gmail.com>
2025-03-25 12:47:58 -0600

    Update logo url

--- LocalSettings.php
+++ LocalSettings.php
@@ -83,8 +83,8 @@ $wgResourceBasePath = $wgScriptPath;
 ## The URL paths to the logo.  Make sure you change this from the default,
 ## or else you'll overwrite your logo when you upgrade!
 $wgLogos = [
-    '1x' => "$wgResourceBasePath/images/sualex-logo.jpg",
-    'icon' => "$wgResourceBasePath/images/sualex-logo.jpg",
+    '1x' => "$wgResourceBasePath/images/sualex-logo.png",
+    'icon' => "$wgResourceBasePath/images/sualex-logo.png",
 ];
 
 #Emailing
